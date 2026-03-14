76ac3851a62d637792004e1ba2a312e5c59834d6
Ata Khatri <atakhatri905@gmail.com>
2026-03-14 18:09:10 +0530

    Delete 30-01-26 directory

--- 30-01-26/examples/abc/simple_pack.java
+++
@@ -1,7 +0,0 @@
-package abc;
-
-public class simple_pack {
-    public static void main(String[] args) {
-        System.out.println("Welcome to package.");
-    }
-}
--- 30-01-26/examples/b.java
+++
@@ -1,9 +0,0 @@
-import mypack1.a;
-
-class b {
-    public static void main(String[] args) {
-        a obj = new a();
-
-        obj.msg();
-    }    
-}
